51d37825ce88466e9114038c1d7652e946a1137a
ASWIN E <151516710+aswine2005@users.noreply.github.com>
2025-03-07 23:30:38 +0530

    Add files via upload

---
+++ Two_sum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> Map=new HashMap();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(Map.containsKey(complement)){
+                return new int[]{
+                    Map.get(complement),i
+                };
+            }
+            Map.put(nums[i],i);
+        }
+        return null;
+
+    }
+}
